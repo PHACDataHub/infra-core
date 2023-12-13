224db5e9f30654e24e1f1a06cd4c4c2ce772b381
Collin Brown <collin.brown@phac-aspc.gc.ca>
2023-12-13 21:43:41 +0000

    feat(vertex): add analytics-bucket gcs bucket

--- vertex-workbench-template/bucket.tf
+++ vertex-workbench-template/bucket.tf
@@ -7,6 +7,23 @@ resource "google_storage_bucket" "bucket" {
   force_destroy               = true
 }
 
+resource "google_storage_bucket" "analytics-bucket" {
+  name     = "analytics-bucket"
+  location = var.region
+  project  = var.project
+  # Enable data versioning
+  versioning {
+    enabled = true
+  }
+  # Disable public access
+  public_access_prevention = "enforced"
+  # Use uniform bucket-level access (i.e. all users in the GCP Project have the same access to the storage bucket)
+  uniform_bucket_level_access = true
+  # Do NOT force destroy buckets with data in them
+  force_destroy = false
+
+}
+
 resource "google_storage_bucket_object" "postscript" {
   name   = "post_startup_script.sh"
   source = "${path.module}/bootstrap_files/post_startup_script.sh"
