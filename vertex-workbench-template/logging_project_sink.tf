31f1356c2c121023049df3b956b37fa9ff05d55f
Collin Brown <collin.brown@phac-aspc.gc.ca>
2024-01-08 17:21:02 +0000

    feat(logging_project_sink.tf): grant log sink storage object creator role. - change filter to be agnostic of resource type

--- vertex-workbench-template/logging_project_sink.tf
+++ vertex-workbench-template/logging_project_sink.tf
@@ -1,7 +1,17 @@
 resource "google_logging_project_sink" "logging_project_sink" {
   project                = var.project
   name                   = var.logging_project_sink_name
-  destination            = "storage.googleapis.com/${google_storage_bucket.bucket.name}"
-  filter                 = "resource.type = gce_instance AND severity >= WARNING"
+  destination            = "storage.googleapis.com/${google_storage_bucket.logging-bucket.name}"
+  filter                 = "severity >= WARNING"
   unique_writer_identity = true
+}
+
+# Because our sink uses a unique_writer, we must grant that writer access to the bucket.
+resource "google_project_iam_binding" "gcs-bucket-writer" {
+  project = var.project
+  role = "roles/storage.objectCreator"
+
+  members = [
+    google_logging_project_sink.logging_project_sink.writer_identity,
+  ]
 }
